5cf665681825b46ff265a8e85e0ec6f4c58a8a24
phil@home <philipp.millner@outlook.com>
2024-06-19 00:34:08 +0200

    feat: exporting icons from one index file

---
+++ src/assets/index.ts
@@ -0,0 +1,8 @@
+import arrow from "../assets/icons/arrow.svg";
+import doneCheck from "../assets/icons/doneCheck.svg";
+import closeCross from "../assets/icons/close.svg";
+import cardsIcon from "../assets/icons/cards.svg";
+import decksIcon from "../assets/icons/decks.svg";
+import emptyIcon from "../assets/icons/empty.svg";
+
+export { arrow, doneCheck, closeCross, cardsIcon, decksIcon, emptyIcon };
